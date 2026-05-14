7267714f58587fc515268aacfdd908c67d618b3e
Md. Nahid Hassan <nahidhassan.ctc@gmail.com>
2026-05-14 12:58:03 +0600

    Add string methods practice in string_methods.py

---
+++ string_methods.py
@@ -0,0 +1,14 @@
+# str practice some string methods
+
+strr = " I Love Python Programming   "
+print (strr.title())
+print (strr.upper())
+print (strr.lower())
+print (strr.strip())
+print (strr.split())
+print (strr.replace("Python","Science"))
+print (strr.find("Python"))
+print (strr.count("o"))
+print ("..........")
+lisst = ["ami" , "tumi" , "se" ,  "amr" , "tader"]
+print (",".join(lisst))
